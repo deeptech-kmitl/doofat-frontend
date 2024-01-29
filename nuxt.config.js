52d067500ff725d402e514bbd51bada487ccdbd5
DevNebulaP <76225632+DevNebulaP@users.noreply.github.com>
2024-01-30 01:55:29 +0700

    Fixed CORS Issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,39 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   ssr: false,
   head: {
-    titleTemplate: '%s - doofat',
-    title: 'doofat',
+    titleTemplate: "%s - doofat",
+    title: "doofat",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
       // {
       //   'http-equiv': 'Content-Security-Policy',
       //   content: 'upgrade-insecure-requests',
       // },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: 'preconnect',
-        href: 'https://fonts.googleapis.com',
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
       },
       {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
         crossorigin: true,
       },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900",
       },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Autour+One&display=swap',
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Autour+One&display=swap",
       },
     ],
     script: [
@@ -49,7 +49,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/vue-qrcode.js', ssr: false }],
+  plugins: [{ src: "~/plugins/vue-qrcode.js", ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -57,19 +57,19 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    '@nuxtjs/auth-next',
-    'nuxt-socket-io',
+    "@nuxtjs/pwa",
+    "@nuxtjs/auth-next",
+    "nuxt-socket-io",
   ],
 
   auth: {
@@ -77,7 +77,7 @@ export default {
     strategies: {
       local: {
         user: {
-          property: 'user',
+          property: "user",
           autoFetch: true,
         },
         token: {
@@ -85,17 +85,28 @@ export default {
           type: false,
         },
         endpoints: {
-          login: { url: process.env.BACKEND_URL + '/api/auth/login', method: 'post' },
-          logout: { url: process.env.BACKEND_URL + '/api/auth/logout', method: 'post' },
-          user: { url: process.env.BACKEND_URL + '/api/auth/user', method: 'get' },
+          login: {
+            url: process.env.BACKEND_URL + "/api/auth/login",
+            method: "post",
+            withCredentials: true,
+          },
+          logout: {
+            url: process.env.BACKEND_URL + "/api/auth/logout",
+            method: "post",
+          },
+          user: {
+            url: process.env.BACKEND_URL + "/api/auth/user",
+            method: "get",
+            withCredentials: true,
+          },
         },
       },
     },
     redirect: {
-      login: '/?signin',
-      logout: '/',
-      callback: '/',
-      home: '/home',
+      login: "/?signin",
+      logout: "/",
+      callback: "/",
+      home: "/home",
     },
   },
 
@@ -112,13 +123,13 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en',
+      lang: "en",
     },
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: false,
       themes: {
@@ -132,9 +143,9 @@ export default {
           success: colors.green.accent3,
         },
         light: {
-          primary: '#FFB900',
-          black: '#1A1A1A',
-          background: '#FFF',
+          primary: "#FFB900",
+          black: "#1A1A1A",
+          background: "#FFF",
         },
       },
     },
@@ -148,4 +159,4 @@ export default {
   //     pathRewrite: { '^/api': '' }
   //   }
   // },
-}
+};
