b198e9a54625e5353bea6272c4c734a2871f23e1
DevNebulaP <76225632+DevNebulaP@users.noreply.github.com>
2024-01-30 00:30:28 +0700

    fixed urls

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,9 +85,9 @@ export default {
           type: false,
         },
         endpoints: {
-          login: { url: '/api/auth/login', method: 'post' },
-          logout: { url: '/api/auth/logout', method: 'post' },
-          user: { url: '/api/auth/user', method: 'get' },
+          login: { url: process.env.BACKEND_URL + '/api/auth/login', method: 'post' },
+          logout: { url: process.env.BACKEND_URL + '/api/auth/logout', method: 'post' },
+          user: { url: process.env.BACKEND_URL + '/api/auth/user', method: 'get' },
         },
       },
     },
@@ -102,7 +102,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.BACKEND_URL,
   },
 
   router: {
